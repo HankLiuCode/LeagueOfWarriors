8bf945afb21d42c65d24df7e869709c272560a66
HankLiu <level1rookie@gmail.com>
2021-08-20 01:52:48 +0800

    Sync Interval Test

--- Assets/Scripts/Projectiles/SkillShot.cs
+++ Assets/Scripts/Projectiles/SkillShot.cs
@@ -4,160 +4,79 @@ using UnityEngine;
 using Mirror;
 using Dota.Core;
 
-namespace Dota.Abilities
+public class SkillShot : NetworkBehaviour
 {
-    // TODO: Make Parent class from skillshot and projectile
-    public class SkillShot : NetworkBehaviour
+    [SerializeField] float speed = 8;
+    [SerializeField] Vector3 direction;
+    [SerializeField] float damage = 50;
+    Vector3 startPos;
+    Vector3 destroyPoint;
+    NetworkIdentity owner;
+
+    // OnTriggerEnter IsSometimes called twice or more, this prevents it
+    bool hasHit;
+
+
+    #region Server
+
+    [Server]
+    public void ServerDealDamageTo(Health health, float damage)
     {
-        [SerializeField] Vector3 direction;
-        [SerializeField] float speed = 8;
-        [SerializeField] float damage = 50;
-
-        [SyncVar]
-        [SerializeField]
-        NetworkIdentity owner;
-
-        [SyncVar]
-        [SerializeField]
-        Vector3 startPos;
-
-        [SyncVar]
-        [SerializeField]
-        Vector3 destroyPoint;
-
-        // OnTriggerEnter Is sometimes called twice or more, this prevents it
-        bool hasHit;
-
-        // Client-Side Collision Detection / Server Spawns
-
-        // Update
-        //     skillShot position update on client side
-
-        // OnCollision
-        //      CmdNotifyHasHit
-
-        // CmdNotifyServerHit()
-
-
-
-
-        #region Both
-
-        private void Update()
-        {
-            if (isServer)
-            {
-                ServerUpdate();
-            }
-
-            if (isClient)
-            {
-                ClientUpdate();
-            }
-        }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (isServer)
-            {
-                ServerOnTriggerEnter(other);
-            }
-
-            if (isClient)
-            {
-                ClientOnTriggerEnter(other);
-            }
-        }
-        #endregion
-
-        #region Server
-
-        [Server]
-        public void ServerDealDamageTo(Health health, float damage)
-        {
-            health.ServerTakeDamage(damage);
-        }
-
-        [Server]
-        public void ServerSetSpeed(float speed)
-        {
-            this.speed = speed;
-        }
-
-        [Server]
-        public void ServerSetOwner(NetworkIdentity owner)
-        {
-            this.owner = owner;
-        }
-
-        [Server]
-        public void ServerSetDamage(float damage)
-        {
-            this.damage = damage;
-        }
-
-        [Server]
-        public void ServerSetDirection(Vector3 startPos, Vector3 direction, float travelDist)
-        {
-            this.direction = new Vector3(direction.normalized.x, 0, direction.normalized.z);
-
-            this.startPos = startPos;
-            destroyPoint = startPos + this.direction.normalized * travelDist;
-        }
-
-        [Server]
-        private void ServerOnTriggerEnter(Collider other)
-        {
-            NetworkIdentity otherIdentity = other.gameObject.GetComponent<NetworkIdentity>();
-            Health health = other.GetComponent<Health>();
-            if (health && !hasHit && otherIdentity != owner)
-            {
-                if (health.IsDead()) { return; }
-
-                hasHit = true;
-                health.ServerTakeDamage(damage);
-                gameObject.SetActive(false);
-            }
-        }
-
-
-        [Server]
-        private void ServerUpdate()
-        {
-            transform.forward = destroyPoint - transform.position;
-
-            transform.position += direction * speed * Time.deltaTime;
-
-            if (Vector3.Distance(startPos, destroyPoint) < Vector3.Distance(startPos, transform.position))
-            {
-                gameObject.SetActive(false);
-            }
-        }
-        #endregion
-
-        #region Client
-
-
-        [Client]
-        private void ClientUpdate()
-        {
-            if (Vector3.Distance(startPos, destroyPoint) < Vector3.Distance(startPos, transform.position))
-            {
-                gameObject.SetActive(false);
-            }
-        }
-
-        private void ClientOnTriggerEnter(Collider other)
-        {
-            NetworkIdentity otherIdentity = other.gameObject.GetComponent<NetworkIdentity>();
-            Health health = other.GetComponent<Health>();
-            if (health && !hasHit && otherIdentity != owner)
-            {
-                //if (health.IsDead()) { return; }
-                hasHit = true;
-                gameObject.SetActive(false);
-            }
-        }
-        #endregion
+        health.ServerTakeDamage(damage);
+        NetworkServer.Destroy(gameObject);
     }
+
+    [Server]
+    public void ServerSetDirection(Vector3 startPos, Vector3 direction, float travelDist)
+    {
+        this.direction = new Vector3(direction.normalized.x, 0, direction.normalized.z);
+        this.startPos = startPos;
+        destroyPoint = startPos + this.direction.normalized * travelDist;
+    }
+
+    [Server]
+    public void ServerSetOwner(NetworkIdentity owner)
+    {
+        this.owner = owner;
+    }
+
+    [Server]
+    public void ServerSetDamage(float damage)
+    {
+        this.damage = damage;
+    }
+
+    [Server]
+    public void ServerSetSpeed(float speed)
+    {
+        this.speed = speed;
+    }
+    
+    [ServerCallback]
+    private void Update()
+    {
+        transform.forward = destroyPoint - transform.position;
+        transform.position += direction * speed * Time.deltaTime;
+
+        if (Vector3.Distance(startPos, destroyPoint) < Vector3.Distance(startPos, transform.position))
+        {
+            NetworkServer.Destroy(gameObject);
+        }
+    }
+
+    [ServerCallback]
+    private void OnTriggerEnter(Collider other)
+    {
+        NetworkIdentity otherIdentity = other.gameObject.GetComponent<NetworkIdentity>();
+        Health health = other.GetComponent<Health>();
+        if (health && !hasHit && otherIdentity != owner)
+        {
+            if (health.IsDead()) { return; }
+
+            hasHit = true;
+            health.ServerTakeDamage(damage);
+            NetworkServer.Destroy(gameObject);
+        }
+    }
+    #endregion
 }
