2684de7ef2891783efbc89b501176e97565e9849
叡 林 <ray810803@yahoo.com.tw>
2021-08-19 17:54:13 +0800

    UI選單進遊戲/遊戲案左上角跳回選單

---
+++ Assets/Game/Scene/menu1.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;///載入函示庫
+
+public class menu1 : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("NetworkSandbox");
+        ///載入遊戲場景
+    }
+
+   public void QuitGame()
+    {
+        Application.Quit();
+        ///結束遊戲
+    }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene("Loby");
+        ///回選單場景
+    }
+
+}
